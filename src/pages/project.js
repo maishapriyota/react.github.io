3c5524ae8b44143a92bf73c834a987294496e62c
Maisha Maimuna <66841427+maishapriyota@users.noreply.github.com>
2024-04-17 08:57:38 +0200

    Update project.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import projectImage from './projectimage.png'; // Import your project image
+
+const Project = () => {
+    
+
+    return (
+        <div>
+            <div className="drop-in-2">
+            <div className="read-more">
+                <h2>Read More About Me</h2>
+                <Link to="/about">
+                    <img src={projectImage} alt="Read More" className="readmore"/>
+                </Link>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
 
