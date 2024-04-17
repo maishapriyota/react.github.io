6c2871aeb50189e299bfe6966fea307d66dd5e2c
Maisha Maimuna <66841427+maishapriyota@users.noreply.github.com>
2024-04-17 08:54:41 +0200

    Update navigation.js

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navigation({ openChatbox  }) {
-  
+function Navigation() {
   return (
-    
     <nav>
       <ul>
-      <li><Link to="/">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
-        {/* Call handleContactClick when Contact link is clicked */}
-        <li><a href="/chatbot" onClick={openChatbox}>Contact</a></li>
+        {/* Use Link component for Contact link */}
+        <li><Link to="/chatbot">Contact</Link></li>
+        <li><Link to="/project">Projects</Link></li>
       </ul>
-      </nav>
+    </nav>
   );
 }
 
